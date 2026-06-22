416ab574b8f5a0eae3bb8e4bd5f1018880774fc2
joaovitorcosta29 <joaovitorcostalima81@gmail.com>
2026-06-22 14:51:10 -0300

    inicializando o projeto

---
+++ src/main/java/com/projetofinalTCC/backendTCC/BackendTccApplication.java
@@ -0,0 +1,13 @@
+package com.projetofinalTCC.backendTCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetofinalTCC/backendTCC/BackendTccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetofinalTCC.backendTCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
